11e00caf232f97eaa7e4ee3b776ecb09d8753e03
Shehzad Aslam <shehzada.pk786@gmail.com>
2020-07-07 23:05:21 +0500

    Stripe integrated.

--- src/components/thankyou/index.js
+++ src/components/thankyou/index.js
@@ -19,43 +19,12 @@ const ThankyouIndex = () => {
 
                         An order confirmation has been emailed.<br/><br/>
 
-                        If you have any questions or thoughts please hit us up through our live chat and one of our experts will be happy to chat with you. +972 2-500-6076<br/><br/>
+                        If you have any questions or thoughts please hit us up through our live chat and one of our experts will be happy to chat with you. +61 450 889 573<br/><br/>
 
                         Bon Appe'tit!!<br/><br/>
                     </h3>
                 </div>
             </div>
-
-
-                {/*<div className="text-holder" style={{ paddingTop: '25px' }}>*/}
-                    {/*<div className="row">*/}
-                        {/*<div className="col-xs-6">*/}
-                            {/*<div className="single-service text-left">*/}
-                                {/*<h4 className="title">תודה!</h4>*/}
-                                {/*<p>ההזמנה שלך התקבלה.</p>*/}
-                                {/*<p>רוכב של המסעדה יהיה במקום שלך בסביבה.</p>*/}
-                                {/*<h4 className="green-heading">test</h4>*/}
-                            {/*</div>*/}
-
-                            {/*<div className="single-service text-left">*/}
-                                {/*<h4 className="title">צריך תמיכה</h4>*/}
-                                {/*<p>ההזמנה שלך מ <br/><strong>test</strong></p>*/}
-                                {/*<a href={'#sa'}><i className={'fa fa-phone'}/>  +972 58-760-6220</a>*/}
-                            {/*</div>*/}
-                        {/*</div>*/}
-                        {/*<div className="col-xs-6">*/}
-                            {/*<div className="single-service text-left">*/}
-                                {/*<h4 className="title">פרטי הזמנה</h4>*/}
-                                {/*<ol className="rectangle-list">*/}
-                                    {/*<li className={'active'}><a href="#sa">State One</a></li>*/}
-                                    {/*<li><a href="#sa">State Two</a></li>*/}
-                                    {/*<li><a href="#sa">State Three</a></li>*/}
-                                    {/*<li><a href="#sa">State Four</a></li>*/}
-                                {/*</ol>*/}
-                            {/*</div>*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
-            {/*</div>*/}
 
         </div>
     );
